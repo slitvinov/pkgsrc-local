17918b788c3a51f4c57cf3efc86f6f25acad16a4
Sergey Litvinov <slitvinov@gmail.com>
2015-09-02 14:16:53 +0200

    Another update of wip/local

--- gts-snapshot/buildlink3.mk
+++ gts-snapshot/buildlink3.mk
@@ -1,4 +1,14 @@
 # $NetBSD$
+# XXX
+# XXX This file was created automatically using createbuildlink-3.16.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.gts?=	build
 
 BUILDLINK_TREE+=	gts
 
@@ -8,9 +18,14 @@ GTS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gts+=	gts>=0.7.6
 BUILDLINK_PKGSRCDIR.gts?=	../../wip/gts-snapshot
 
-
-.include "../../devel/glib2/buildlink3.mk"
-
+# XXX
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+#.include "../../devel/glib2/buildlink3.mk"
+#.include "../../graphics/netpbm/buildlink3.mk"
 .endif	# GTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gts
