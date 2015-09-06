2d6063d741066478747abea7c1328f94b6e01306
Sergey Litvinov <slitvinov@gmail.com>
2015-09-06 15:50:48 +0200

    Rearanged buildlink deps

--- gerris-snapshot/buildlink3.mk
+++ gerris-snapshot/buildlink3.mk
@@ -7,16 +7,10 @@ GERRIS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gerris+=	gerris>=0.9.2.131206
 BUILDLINK_PKGSRCDIR.gerris?=	../../wip/gerris-snapshot
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/netcdf/buildlink3.mk"
-#.include "../../math/gsl/buildlink3.mk"
-#.include "../../wip/gts-snapshot/buildlink3.mk"
-#.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/netcdf/buildlink3.mk"
+.include "../../math/gsl/buildlink3.mk"
+.include "../../wip/gts-snapshot/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .endif	# GERRIS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gerris
--- gts-snapshot/buildlink3.mk
+++ gts-snapshot/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.16.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.gts?=	build
 
 BUILDLINK_TREE+=	gts
 
@@ -18,14 +8,8 @@ GTS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gts+=	gts>=0.7.6
 BUILDLINK_PKGSRCDIR.gts?=	../../wip/gts-snapshot
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/glib2/buildlink3.mk"
-#.include "../../graphics/netpbm/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/netpbm/buildlink3.mk"
 .endif	# GTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gts
