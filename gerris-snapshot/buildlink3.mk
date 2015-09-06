93e425aadad23b5e4b7dc08114b8b7d631de7a06
Sergey Litvinov <slitvinov@gmail.com>
2015-09-06 15:56:48 +0200

    Rearanged buildlink deps

--- gerris-snapshot/buildlink3.mk
+++ gerris-snapshot/buildlink3.mk
@@ -10,7 +10,7 @@ BUILDLINK_PKGSRCDIR.gerris?=	../../wip/gerris-snapshot
 .include "../../devel/netcdf/buildlink3.mk"
 .include "../../math/gsl/buildlink3.mk"
 .include "../../wip/gts-snapshot/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
+q.include "../../devel/glib2/buildlink3.mk"
 .endif	# GERRIS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gerris
